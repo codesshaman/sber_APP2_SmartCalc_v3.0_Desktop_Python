4b1b76b4a6a2af0798ac3870f39e779cf5ee969b
codesshaman <nicksemail@yandex.ru>
2022-12-23 11:27:40 +0300

    CalcWindow class

--- src/views/main.py
+++ src/views/main.py
@@ -9,22 +9,20 @@ class CalcWindow(QMainWindow):
         self.setWindowTitle("SmartCalc v3.0.0")
         self.setGeometry(500, 300, 450, 300)
         # Окно для цифр
-        main_text = QtWidgets.QLabel(self)
-        main_text.setText("1234567890")
-        main_text.move(10, 10)
-        main_text.setFixedWidth(480)
+        self.main_text = QtWidgets.QLabel(self)
+        self.main_text.setText("1234567890")
+        self.main_text.move(10, 10)
+        self.main_text.setFixedWidth(480)
         # Кнопки
-        btn = QtWidgets.QPushButton(self)
-        btn.move(10, 70)
-        btn.setFixedWidth(65)
-        btn.setText("C")
-        btn.clicked.connect(btn_fnk)
+        self.btn = QtWidgets.QPushButton(self)
+        self.btn.move(10, 70)
+        self.btn.setFixedWidth(65)
+        self.btn.setText("C")
+        self.btn.clicked.connect(btn_fnk)
 
 def calculator():
     app = QApplication(sys.argv)
-    window = QMainWindow()
-    # Заголовок и отступы
-
+    window = CalcWindow()
     window.show()
     sys.exit(app.exec())
 
