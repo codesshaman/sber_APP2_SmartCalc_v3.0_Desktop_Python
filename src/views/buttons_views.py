c1e0309d7cf2ec2870caa3f7958c39f680d5137b
codesshaman <nicksemail@yandex.ru>
2022-12-26 16:11:58 +0300

    add classes description

--- src/views/buttons_views.py
+++ src/views/buttons_views.py
@@ -1,11 +1,28 @@
+from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QPushButton
-
+from controllers.buttons_actions import *
+# Класс маленькой кнопки
 class SimpleButton(QPushButton):
+    "Класс обычной короткой кнопки"
+    # Инициализация класса
     def __init__(self):
-        
+        self.btn = QtWidgets.QPushButton(self)
+        self.btn.move(10, 70)
+        self.btn.setFixedWidth(65)
+        self.btn.setText("C")
+        self.btn.clicked.connect(btn_fnk)
 
-    btn = (window)
-    btn.move(10, 70)
-    btn.setFixedWidth(65)
-    btn.setText("C")
-    btn.clicked.connect(function)
+    # Назначение координат кнопки
+    def coordinates(self, x, y):
+        self.btn.move(x, y)
+
+    # Назначение цвета кнопки
+
+    # Назначение функции кнопки
+    def action(self, btn_function):
+        self.btn.clicked.connect(btn_function)
+
+# # Класс большой кнопки
+# class LongButton(QPushButton):
+def little_btn():
+    btn = SimpleButton()
--- src/views/main.py
+++ src/views/main.py
@@ -1,14 +1,19 @@
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from controllers.buttons_actions import *
+from buttons_views import *
 import sys
 
+# Класс основного окна
 class CalcWindow(QMainWindow):
+    "Класс основного окна приложения"
+    # Инициализация класса
     def __init__(self):
+        # Заголовок окна
         super(CalcWindow, self).__init__()
         self.setWindowTitle("SmartCalc v3.0.0")
         self.setGeometry(500, 300, 450, 300)
-        # Окно для цифр
+        # Цифровое поле
         self.main_text = QtWidgets.QLabel(self)
         self.main_text.setText("1234567890")
         self.main_text.move(10, 10)
