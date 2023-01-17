c059540030edebb0d22b70a19ef01a3ea59027fe
codesshaman <nicksemail@yandex.ru>
2023-01-17 16:58:33 +0300

    timestamp tests

--- src/presenter/buttons_functions/logs.py
+++ src/presenter/buttons_functions/logs.py
@@ -1,4 +1,6 @@
-import os.path
+import os
+import time
+import datetime
 
 
 class CalcLogs():
@@ -6,6 +8,22 @@ class CalcLogs():
     def __init__(self,
                  file_path='logs/logs.py'):
         self.file_path = file_path
+
+    def clean_old_logs(self, h):
+        if h < 1:
+            h = 1
+        else:
+            h = 1 * 1000000
+        nowtime = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+        rotation_time = datetime.datetime.now()
+        for root, dirs, files in os.walk("logs"):
+            for filename in files:
+                if filename < nowtime:
+                    now_h = int(time.time_ns() - (3600000 * h))
+                    now_ms = (now_h / 1000000000)
+                    print(datetime.datetime.utcfromtimestamp(now_ms).strftime('%Y-%m-%d-%H-%M-%S'))
+                    # print(now_ms)
+                    # print(filename)
 
     def check(self):
         exist = os.path.exists(self.file_path)
@@ -32,3 +50,6 @@ class CalcLogs():
         file = open(self.file_path, "a")
         file.write(str(value) + '\n')
         file.close()
+
+func = CalcLogs()
+func.clean_old_logs(0)
